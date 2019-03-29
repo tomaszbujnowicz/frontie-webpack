9986c1a65a89764bd2f28028e08992eea7378f82
Tomasz Bujnowicz <tomasz@prollective.com>
2019-03-29 21:58:53 +0000

    Place more details about the carousel module

--- src/modules/carousel/carousel.js
+++ src/modules/carousel/carousel.js
@@ -1,5 +1,6 @@
 /**
  * Carousel
+ * https://flickity.metafizzy.co/
  */
 
 import Flickity from 'flickity';
@@ -10,6 +11,8 @@ export function carousel() {
   const carousel = document.querySelector('.js-carousel');
 
   if (carousel) {
+    // Flickity - fade in for no FOUC, vanilla JS
+    // https://codepen.io/desandro/pen/JGoGpm
     carousel.classList.remove('is-hidden');
     carousel.offsetHeight;
     new Flickity( carousel, {
