d757d333f40b836123ca96c5c1bcffce3794c402
Tomasz Bujnowicz <tomasz@prollective.com>
2019-03-29 21:55:38 +0000

    Fix indentation

--- src/scripts/components/toggle.js
+++ src/scripts/components/toggle.js
@@ -7,34 +7,34 @@ export function toggle() {
 
   // Show an element
   var show = function (elem) {
-  	elem.classList.remove('is-hidden');
+    elem.classList.remove('is-hidden');
   };
 
   // Hide an element
   var hide = function (elem) {
-  	elem.classList.add('is-hidden');
+    elem.classList.add('is-hidden');
   };
 
   // Toggle element visibility
   var toggle = function (elem) {
-  	elem.classList.toggle('is-hidden');
+    elem.classList.toggle('is-hidden');
   };
 
   // Listen for click events
   document.addEventListener('click', function (event) {
 
-  	// Make sure clicked element is our toggle
-  	if (!event.target.classList.contains('js-toggle')) return;
+    // Make sure clicked element is our toggle
+    if (!event.target.classList.contains('js-toggle')) return;
 
-  	// Prevent default link behavior
-  	event.preventDefault();
+    // Prevent default link behavior
+    event.preventDefault();
 
-  	// Get the content
-  	var content = document.querySelector(event.target.hash);
-  	if (!content) return;
+    // Get the content
+    var content = document.querySelector(event.target.hash);
+    if (!content) return;
 
-  	// Toggle the content
-  	toggle(content);
+    // Toggle the content
+    toggle(content);
 
   }, false);
 
