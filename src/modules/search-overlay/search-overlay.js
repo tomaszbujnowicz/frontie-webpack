a45e01c0583bb16a9bd17758fac4b2288c697e0d
Tomasz Bujnowicz <tomasz@prollective.com>
2019-05-24 15:12:36 +0100

    Add Search Overlay Module

---
+++ src/modules/search-overlay/search-overlay.js
@@ -0,0 +1,48 @@
+/**
+ * Search Overlay
+ */
+
+ import { show, hide, toggle } from '../../scripts/lib/visibility.js'
+
+export default function searchOverlay() {
+
+  const target = document.querySelector('[data-search-overlay-target]');
+  const input = target.querySelector('[data-search-overlay-input]')
+
+  var showOverlay = function() {
+    show(target);
+    input.focus();
+    document.body.classList.add('overflow-hidden');
+	};
+
+  var closeOverlay = function() {
+    hide(target);
+    document.body.classList.remove('overflow-hidden');
+  };
+
+  document.addEventListener('click', function(e) {
+
+    // Open an overlay
+  	if (e.target.matches('[data-search-overlay-trigger]')) {
+      e.preventDefault();
+  		showOverlay();
+  	}
+
+    // Close an overlay
+    if (e.target.matches('[data-search-overlay-close]')) {
+      e.preventDefault();
+  		closeOverlay();
+  	}
+
+  }, false);
+
+  document.addEventListener('keydown', function(e) {
+
+    // Close an overlay on Escape key click
+    if (e.key === 'Escape' || e.keyCode === 27) {
+      closeOverlay();
+    }
+
+  });
+
+}
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,6 +9,7 @@ import './lib/polyfill-closest.js';
 // Modules
 import navbar from '../modules/navbar/navbar';
 import carousel from '../modules/carousel/carousel';
+import searchOverlay from '../modules/search-overlay/search-overlay';
 
 // Components
 import collapse from './components/collapse.js';
@@ -20,6 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Modules
   navbar();
   carousel();
+  searchOverlay();
 
   // Components
   collapse();
